5cbeeb8712cae71c0cb424711a0aaff9609b9d12
4urora3night <auroraph4ntom@gmail.com>
2025-07-11 17:17:03 +0100

    Imported refined dependencies checker from moon.

--- earth.sh
+++ earth.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 # by 4urora3night
-set -euo pipefail
+
+# -- Precautions -- #
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cleanup() {
+  [[ -d "${script_dir}/cache" ]] && rm -rf "${script_dir}/cache"
+}
+trap cleanup EXIT
+
+
+
+# -- Variables -- #
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+dependencies_apps=("fzf" "gum" "yq" "fd" "bat" "flatpak")
+dependencies_files=("app-installer.sh" "aur-helper.sh" "settings.sh" "utils.sh")
+text_box_size=$(($(tput cols) - 4))
+config_toml=Null
 
-# -Import functions- #
+# -- Dependency Checks -- #
 
-source "${script_dir}/lib/settings.sh"
-source "${script_dir}/lib/app-installer.sh"
-source "${script_dir}/lib/utils.sh"
-source "${script_dir}/lib/aur-helper.sh"
+for file in "${dependencies_files[@]}"; do
+  [ ! -e "${script_dir}/lib/${file}" ] && echo "FILE NOT FOUND: ${file}" && exit 1
+  source "${script_dir}/lib/${file}"
+done
+
 
 if [[ "$(tty)" == "/dev/tty"* ]]; then
   source "${script_dir}/lib/tty-ui.sh"
@@ -16,39 +36,11 @@ else
   source "${script_dir}/lib/terminal-ui.sh"
 fi
 
-# --  Functions  -- #
-cleanup() {
-  [[ -d "${script_dir}/cache" ]] && rm -rf "${script_dir}/cache"
-}
-
-install_dependencies() {
-  local dependencies=("fzf" "gum" "yq" "fd" "bat" "flatpak") 
-  local dependencies_install=()
-  local update=1
-  for i in "${dependencies[@]}"; do
-    if check_app_installed "${i}"; then
-      dependencies_install+=("${i}")
-      update=0
-    fi
-  done
-  if [[ "${update}" -eq 0 ]]; then
-    echo -e "Installing dependencies"
-    for i in "${dependencies_install[@]}"; do
-      echo " - ${i}"
-    done
-    sleep 1
-    sudo pacman -Sy
-    pacman_install "${dependencies_install[@]}"
-  fi
-}
-
-# --  Variable  -- #
-text_box_size=$(($(tput cols) - 4))
-config_toml=Null
-trap cleanup EXIT
-
-install_dependencies
+dependencies_app_checks
 aur_helper_checks
+
+# -- Main Loop -- #
+
 while true; do
   clear
   title
