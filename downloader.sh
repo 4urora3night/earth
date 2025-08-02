be0b06325736ea248f1e69f439d3e20023a9be9d
4urora3night <auroraph4ntom@gmail.com>
2025-08-02 10:01:55 +0100

    chore:add missing file to download.sh

--- downloader.sh
+++ downloader.sh
@@ -8,9 +8,10 @@ chmod +x earth.sh
 
 cd lib
 
-curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/app-installer.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/toml-config-installer.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/aur-helper.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/settings.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/terminal-ui.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/tty-ui.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/utils.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/distro-lib/arch-linux-utils.sh
