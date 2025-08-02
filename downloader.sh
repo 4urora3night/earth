6f63c6179e5bd4c356999a2533f06fe123f9bf42
4urora3night <auroraph4ntom@gmail.com>
2025-08-02 10:06:22 +0100

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p earth
 mkdir -p earth/lib
+mkdir -p earth/lib/distro-lib
 cd earth
 
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/earth.sh
@@ -14,4 +15,7 @@ curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/terminal-ui.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/tty-ui.sh
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/utils.sh
+
+cd distro-lib
+
 curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/distro-lib/arch-linux-utils.sh
