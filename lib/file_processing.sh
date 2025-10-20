a4d1d9ac9822c82c0be3d9ce96c9ec9990860181
4urora3night <auroraph4ntom@gmail.com>
2025-10-20 20:35:58 +0100

    feat: initial commit of file processing

--- lib/file_processing.sh
+++ lib/file_processing.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+available_tables=()
+
+process_toml() {
+  local file="$1"
+  local tables=(".pacman.install" ".flatpak.install" ".git.clone" ".git.location" ".wget.download" ".wget.location")
+
+  for table in "${tables[@]}"; do
+    if tomlq -r "${table}" "${file}" | grep -q .; then
+      available_tables+=("${table}")
+    fi
+  done
+
+  if [[ -z "${available_tables[*]}" ]]; then
+    log_error "No expected tables found"
+    exit 1
+  fi
+
+  log_information "Tables found:${available_tables[*]}"
+}
